930e75d52b89044231edfa448e48733a72c41987
Shannon Foster <shannon.foster@lunarline.com>
2018-08-15 17:47:53 -0400

    The code

--- Basic Calculator/Program.cs
+++ Basic Calculator/Program.cs
@@ -4,13 +4,32 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Basic_Calculator
+namespace FizzBuzz
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("To get started, enter an integer value for x and y.");
+            Console.WriteLine("To exit, type exit.");
+            do
+            {
+                Console.Write("x = ");
+                string str = Console.ReadLine();
+                if (str == "exit") {
+                    Console.WriteLine("Exiting..");
+                    break;
+                 }
+
+                int x = Convert.ToInt32(str);
+
+                Console.Write("y = ");
+                int y = Convert.ToInt32(Console.ReadLine());
+
+                int sum = x + y;
+                Console.WriteLine("Result: {0}", sum);
+            } while (true);
+
             Console.ReadKey();
         }
     }
